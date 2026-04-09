c984f549a5e417e5e4279647e7bee3373d0eda63
KR0nyX629 <sen0011@manorlakesp12.vic.edu.au>
2026-04-09 11:06:17 +1000

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,75 @@
-
+function popup() {
+    chrome.runtime.sendMessage({ action: "popup" });
+}
+var active = true;
+var level = 1;
+var timeout = 7;
+document.addEventListener('DOMContentLoaded', function () {
+    var autoBool = document.getElementById("autoCheckbox");
+    var timeoutInput = document.getElementById("timeout");
+    var button = document.getElementById("button");
+    var selectLevel = document.getElementById("selectLevel");
+    var donateText = document.getElementById('donateText');
+    var donateImg = document.getElementById('donateImg');
+    if (donateImg && donateText) {
+        donateText.addEventListener('mouseover', function () {
+            donateImg.style.visibility = 'visible';
+        });
+    }
+    if (button) {
+        button.addEventListener("click", function () {
+            disableButton(button);
+            popup();
+        });
+    }
+    if (autoBool) {
+        chrome.storage.sync.get("active", function (result) {
+            if (result.active) {
+                active = true;
+                autoBool.checked = active;
+            }
+        });
+        autoBool.addEventListener("click", function () {
+            active = autoBool.checked;
+            chrome.storage.sync.set({ "active": active });
+            if (active) {
+                checkLastOpenedPopup();
+            }
+        });
+    }
+    if (timeoutInput) {
+        chrome.storage.sync.get("timeout", function (result) {
+            if (result.timeout) {
+                timeout = parseInt(result.timeout);
+                timeoutInput.value = result.timeout;
+            }
+        });
+        timeoutInput.addEventListener("change", function () {
+            chrome.storage.sync.set({ "timeout": timeoutInput.value });
+            timeout = parseInt(timeoutInput.value);
+        });
+    }
+    if (selectLevel) {
+        chrome.storage.sync.get("level", function (result) {
+            if (result.level) {
+                level = parseInt(result.level);
+                selectLevel.value = result.level;
+            }
+        });
+        selectLevel.addEventListener("change", function () {
+            chrome.storage.sync.set({ "level": selectLevel.value });
+            level = parseInt(selectLevel.value);
+        });
+    }
+});
+function disableButton(button) {
+    button.disabled = true;
+    button.classList.replace("btn-success", "btn-fail");
+    setTimeout(function () {
+        button.disabled = false;
+        button.classList.replace("btn-fail", "btn-success");
+    }, 1000 + (level * 10 * timeout * 1000));
+}
+function checkLastOpenedPopup() {
+    chrome.runtime.sendMessage({ action: "check" });
+}
